83a2dac469f402dc4ee1df0d055cb42abead6936
lubarog13 <lubarog13@gmail.com>
2022-03-20 09:21:02 +0300

    Фильтрация списка по дате

--- lib/results.dart
+++ lib/results.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:neural_project/entities.dart' as entities;
@@ -22,10 +23,11 @@ class ResultsPage extends StatefulWidget {
 
 class _ResultsState extends State<ResultsPage> {
   final _experiments = <entities.Experiment>[];
-  final _collect_experiments = <entities.Experiment>[];
+  var _filtered_experiments = <entities.Experiment>[];
   entities.User? _user;
   bool loaded = true;
   bool only_results = false;
+  var selectedDate = null;
 
   @override
   void initState()  {
@@ -53,36 +55,62 @@ class _ResultsState extends State<ResultsPage> {
     ));
     await firestoreInstance.collection("result").get().then((value) => value.docs.forEach((element) {
       element["experiment"].get().then((value) {
-        setResultDates(value);
+        setResultDates(value, element.data()["date"]);
       });
     }));
+    _filtered_experiments = _experiments;
     loaded = false;
     setState(() {
     });
   }
 
-  setResultDates(value) {
+  setResultDates(value, date) {
     var experiment  = _experiments.firstWhereOrNull((element) => element.date == value["date"]);
     if (null != experiment ) {
-      experiment.resultDate = value["date"];
+      experiment.resultDate = date;
       setState(() {
 
       });
     }
   }
+
+  void showDatePicker()
+  {  showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext builder) {
+        return Container(
+          height: MediaQuery.of(context).copyWith().size.height*0.25,
+          color: Colors.white,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.date,
+            onDateTimeChanged: (value) {
+              if (value != null && value != selectedDate)
+                setState(() {
+                  selectedDate = value;
+                  filterResults(only_results);
+                });
+            },
+            initialDateTime: selectedDate ?? DateTime.now(),
+          ),
+        );
+      }
+  );
+  }
   
   filterResults(bool set_filter) {
     only_results = set_filter;
     if(set_filter) {
-      _collect_experiments.clear();
-      _collect_experiments.addAll(_experiments);
-      _experiments.clear();
-      _experiments.addAll(_collect_experiments.where((element) => element.resultDate!=null));
+      _filtered_experiments = _experiments.where((element) => element.resultDate!=null).toList();
     } else {
-      _experiments.clear();
-      _experiments.addAll(_collect_experiments);
+      _filtered_experiments = [..._experiments];
     }
-    
+    if(selectedDate!=null) {
+      var format = DateFormat("dd.MM.yyyy");
+      _filtered_experiments = _filtered_experiments.where((element) => format.format(DateTime.fromMillisecondsSinceEpoch(element.date.millisecondsSinceEpoch))== format.format(selectedDate)).toList();
+    }
+    setState(() {
+
+    });
   }
 
   @override
@@ -121,21 +149,19 @@ class _ResultsState extends State<ResultsPage> {
                         width: 100,
                         margin: const EdgeInsets.only(right: 11),
                         child: Text(
-                          "Только заверщенные исследования",
+                          "Только завершенные исследования",
                           style: GoogleFonts.openSans(fontWeight: FontWeight.w400, fontSize: 14, color: Colors.white),
                           textAlign: TextAlign.end,
                         ),
                       ),
-                      CupertinoSwitch(value: only_results, onChanged: (change) { filterResults(change) ; setState(() {
-
-                      });  }, activeColor: const Color(0xFF3D7ECE),),
+                      CupertinoSwitch(value: only_results, onChanged: (change) { filterResults(change) ;}, activeColor: const Color(0xFF3D7ECE),),
                       Container(
                         margin: const EdgeInsets.only(left: 41),
                         child: ButtonTheme(
                             minWidth: 157,
                             height: 41,
                             child: OutlinedButton(
-                              onPressed: () => {},
+                              onPressed: () => { showDatePicker() },
                               style: OutlinedButton.styleFrom(
                                 minimumSize: const Size(118, 47),
                                 side: const BorderSide(
@@ -155,16 +181,44 @@ class _ResultsState extends State<ResultsPage> {
                     ],
                   ),
                   Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          selectedDate!=null? DateFormat("dd.MM.yyyy").format(selectedDate): "",
+                          style: GoogleFonts.openSans(fontWeight: FontWeight.w600, color: Colors.white, fontSize: 14),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          child: GestureDetector(
+                          child: Text(
+                            selectedDate!=null? "Все": "",
+                            style: GoogleFonts.openSans(fontWeight: FontWeight.w600,
+                                color: const Color(0xFF307EDE),
+                                fontSize: 16,
+                                decoration: TextDecoration.underline,
+                                decorationColor: const Color(0xFF307EDE)
+                            ),
+                          ),
+                            onTap: () { selectedDate = null; filterResults(only_results);},
+                          )
+                        )
+                      ]
+                    ),
+                  ),
+                  Container(
                     height: MediaQuery.of(context).size.height - 258,
                       width: MediaQuery.of(context).size.width - 72,
                   child: ListView.builder(
                     scrollDirection: Axis.vertical,
-                    itemCount: _experiments.length,
+                    itemCount: _filtered_experiments.length,
                       itemBuilder: (context, index) {
                     return Container(
                       padding: EdgeInsets.only(left: 22, right: 11),
                       height: 80,
-                      margin: const EdgeInsets.only(top: 39),
+                      margin:  EdgeInsets.only(top: index!=0? 39:0),
                       decoration: const BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.all(Radius.circular(16)),
@@ -177,13 +231,13 @@ class _ResultsState extends State<ResultsPage> {
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text(
-                                DateFormat("dd.MM.yyyy").format(DateTime.fromMillisecondsSinceEpoch(_experiments[index].date.millisecondsSinceEpoch)),
+                                DateFormat("dd.MM.yyyy").format(DateTime.fromMillisecondsSinceEpoch(_filtered_experiments[index].date.millisecondsSinceEpoch)),
                                 style: GoogleFonts.openSans(fontSize: 12, color: const Color(0xFF434343)),
                               ),
                               Container(
                                 margin: EdgeInsets.only(top: 6),
                                 child: Text(
-                                  "Врач ${_experiments[index].doctor.first_name} ${_experiments[index].doctor.last_name}",
+                                  "Врач ${_filtered_experiments[index].doctor.first_name} ${_filtered_experiments[index].doctor.last_name}",
                                   style: GoogleFonts.openSans(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black),
                                 ),
                               )
@@ -191,8 +245,8 @@ class _ResultsState extends State<ResultsPage> {
                           ),
                           Spacer(),
                           Text(
-                            _experiments[index].resultDate==null? "Результат\n не готов": "Результат от\n ${DateFormat("dd.MM.yyyy").format(DateTime.fromMillisecondsSinceEpoch(_experiments[index].resultDate!.millisecondsSinceEpoch))}",
-                            style: GoogleFonts.openSans(color: Color(0xFF5A5A5A), fontSize:  _experiments[index].resultDate==null? 15: 13, fontWeight: FontWeight.w600),
+                            _filtered_experiments[index].resultDate==null? "Результат\n не готов": "Результат от\n ${DateFormat("dd.MM.yyyy").format(DateTime.fromMillisecondsSinceEpoch(_filtered_experiments[index].resultDate!.millisecondsSinceEpoch))}",
+                            style: GoogleFonts.openSans(color: Color(0xFF5A5A5A), fontSize:  _filtered_experiments[index].resultDate==null? 15: 13, fontWeight: FontWeight.w600),
                           )
                         ],
                       ),
